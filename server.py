998de00a687f1c42b5172fd11f08686e76f756a9
Zach Mullen <zach.mullen@kitware.com>
2017-06-01 15:44:51 -0400

    Add endpoint for listing history of file revisions

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import json
 from girder import constants, events
+from girder.api import access
+from girder.api.describe import Description, autoDescribeRoute
+from girder.api.rest import boundHandler, RestException
 from girder.utility.model_importer import ModelImporter
 
 
@@ -41,6 +44,23 @@ def _handleRevisionUpload(event):
         }, multi=False)
 
 
+@access.public(scope=constants.TokenScope.DATA_READ)
+@boundHandler()
+@autoDescribeRoute(
+    Description('Retrieve a list of ')
+    .param('path', 'Canonical path of the versioned file.')
+    .pagingParams(
+        defaultSort='meta.versionedFileRevision', defaultSortDir=constants.SortDir.DESCENDING)
+)
+def _getRevisionsByPath(self, path, limit, offset, sort, params):
+    cursor = self.model('item').find({
+        'meta.versionedFilePath': {'$eq': path}
+    }, sort=sort)
+    return list(self.model('item').filterResultsByPermission(
+        cursor=cursor, user=self.getCurrentUser(), level=constants.AccessType.READ, limit=limit,
+        offset=offset))
+
+
 def load(info):
     events.bind('model.file.finalizeUpload.after', info['name'], _handleRevisionUpload)
 
@@ -48,3 +68,5 @@ def load(info):
          ('meta.versionedFilePath', constants.SortDir.ASCENDING),
          ('meta.versionedFileRevision', constants.SortDir.DESCENDING)
     ], {}))
+
+    info['apiRoot'].item.route('GET', ('revisions',), _getRevisionsByPath)
